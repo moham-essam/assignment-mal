49081cd190c4fab7e344b5f707be1ab30f3f2e42
Mohamed Essam <miessam16@gmail.com>
2026-08-29 09:12:14 +0400

    Fix E6 E8 values

--- src/main/java/com/mal/assignment/simulation/ScenarioFixture.java
+++ src/main/java/com/mal/assignment/simulation/ScenarioFixture.java
@@ -21,9 +21,9 @@ public final class ScenarioFixture {
                 new ScheduledCommand(2, new AuthorizeCommand("E3", "ACC-001", "Auth-A", 20_000L)),
                 new ScheduledCommand(3, new BookCreditCommand("E4", "ACC-001", 3, 40_000L, "E4")),
                 new ScheduledCommand(4, new SettleAuthorizationCommand("E5", "ACC-001", "Auth-A", 4, 18_500L)),
-                new ScheduledCommand(4, new SettleAuthorizationCommand("E6", "ACC-001", "Auth-Z", 4, 18_500L)),
+                new ScheduledCommand(4, new SettleAuthorizationCommand("E6", "ACC-001", "Auth-Z", 4, 18_000L)),
                 new ScheduledCommand(5, new BookDebitCommand("E7", "ACC-001", 2, 62_000L, "E7")),
-                new ScheduledCommand(5, new AuthorizeCommand("E8", "ACC-001", "Auth-B", 90_000L)),
+                new ScheduledCommand(5, new AuthorizeCommand("E8", "ACC-001", "Auth-B", 90_00L)),
                 new ScheduledCommand(5, new CreditInstalmentsCommand("E10", "ACC-002", 5, 10_000L, 3)),
                 new ScheduledCommand(6, new ReverseTransactionCommand("E9", "ACC-001", "E7"))
         );
